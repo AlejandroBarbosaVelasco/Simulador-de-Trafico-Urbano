a11543eab8eb545c2611fc6e158e68c35fb67542
Carlos Alvarez <carlosadrian.aa@gmail.com>
2025-11-25 12:13:43 -0600

    Arreglo direccion de imagenes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # main.py
 import pygame
+import os
+import sys
+
 from city_map import (
     GRID_SIZE, 
     BLOCK_SIZE, 
@@ -7,6 +10,10 @@ from city_map import (
     generate_traffic_lights,
     generate_vehicles
 )
+
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+path_hori = os.path.join(script_dir, "Calle Hori.png")
+path_vert = os.path.join(script_dir, "Calle Verti.png")
 
 pygame.init()
 
@@ -20,8 +27,8 @@ pygame.display.set_caption("Ciudad 12x12 con Semáforos Independientes")
 GREEN = (80, 170, 60)
 
 # Cargar imágenes de calles
-img_hori = pygame.image.load("Calle Hori.png")
-img_vert = pygame.image.load("Calle Verti.png")
+img_hori = pygame.image.load(path_hori)
+img_vert = pygame.image.load(path_vert)
 
 img_hori = pygame.transform.scale(img_hori, (BLOCK_SIZE, STREET_WIDTH))
 img_vert = pygame.transform.scale(img_vert, (STREET_WIDTH, BLOCK_SIZE))
